1f8c6de560af7ee51bbe937aebd5fab04c518465
Renato Pacheco <renato.pacheco@unip.br>
2026-08-06 14:00:29 -0300

    feat: Incluindo cache para ambos storages

--- src/app/core/storage/local-storage.service.ts
+++ src/app/core/storage/local-storage.service.ts
@@ -4,20 +4,33 @@ import { appConfig } from "@app/app.config";
 @Injectable({ providedIn: 'root' })
 export class LocalStorageService {
 
+  private cache: Record<string, any> = {};
+  private getCache(key: string): any {
+    if (typeof this.cache[key] === 'boolean' 
+      || typeof this.cache[key] === 'number' 
+      || typeof this.cache[key] === 'string') {
+      return this.cache[key];
+    }
+    return this.cache[key] || null;
+  }
+
   setItem(key: string, value: any): void {
+    this.cache[key] = value;
     localStorage.setItem(`${appConfig.guid}/${key}`, JSON.stringify(value));
   }
 
   getItem<T>(key: string): T | null {
     const item = localStorage.getItem(`${appConfig.guid}/${key}`);
-    return item ? JSON.parse(item) as T : null;
+    return item ? JSON.parse(item) as T : this.getCache(key);
   }
 
   removeItem(key: string): void {
+    delete this.cache[key];
     localStorage.removeItem(`${appConfig.guid}/${key}`);
   }
 
   clear(): void {
+    this.cache = {};
     localStorage.clear();
   }
 
